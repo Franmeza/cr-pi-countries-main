e3570601dbd386a0dd4bca92810efedc3546f97f
FranMeza <francisco.meza@crowdcontent.com>
2023-08-11 14:01:23 -0500

    db name added to .env file

--- server/src/controllers/activities/getActivities.js
+++ server/src/controllers/activities/getActivities.js
@@ -2,7 +2,6 @@ const { Activity } = require("../../db");
 
 const getActivities = async () => {
   const activities = await Activity.findAll();
-  if(activities.length === 0) throw Error ("No activities to display")
   return activities;
 };
 
--- server/src/db.js
+++ server/src/db.js
@@ -4,11 +4,11 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
 //CONECTION WITH DATABASE
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countriesdb_280d`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, 
   native: false, 
 });
