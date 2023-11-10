96bfd38d5929ff68b85104f9f4c67d97a6fe8cb9
Franmeza <ing.meza.francisco@gmail.com>
2023-11-09 20:22:08 -0500

    forced sync

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 // const axios = require("axios");
 const server = require("./src/server");
 // require("dotenv").config();
-const { conn } = require('./src/db.js');
-const PORT = process.env.PORT 
-const countriesLoader = require('./src/utils/loader')
+const { conn } = require("./src/db.js");
+const PORT = process.env.PORT;
+const countriesLoader = require("./src/utils/loader");
 
 // const {Country} = conn.models
 
-countriesLoader()
-conn.sync({ force: false }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+countriesLoader();
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
