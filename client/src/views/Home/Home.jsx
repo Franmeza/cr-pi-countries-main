f5f7c43506a09d1ad269ea9a88ff2069e04e42db
Franmeza <ing.meza.francisco@gmail.com>
2023-11-13 08:12:18 -0500

    alert for loading time

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -15,6 +15,7 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import SidePanelMobile from "../../components/SidePanelMobile/SidePanelMobile";
 import Loader from "../../components/Loader/Loader";
+import Swal from "sweetalert2";
 
 function Home() {
   const countries = useSelector((state) => state.countries);
@@ -22,7 +23,7 @@ function Home() {
   const [displayPanel, setDisplayPanel] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [aux, setAux] = useState(false);
-  const [loader, setLoader] = useState(false);
+  // const [loader, setLoader] = useState(false);
   const itemsPerPage = 10;
   const totalPages = Math.ceil(countries.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -31,12 +32,30 @@ function Home() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    setLoader(true);
-    const begin = async () => {
-      if (countries.length === 0) await dispatch(fetchCountriesInfo());
-      setLoader(false);
+    // setLoader(true);
 
-      if (activities.length === 0) await dispatch(getActivities());
+    const begin = async () => {
+      try {
+        if (countries.length === 0 || activities.length === 0) {
+          Swal.fire({
+            title: "Please Wait...",
+            html: "Give it some time to load as this app is hosted in a free hosting service. If it takes too much time, please reload the page. Thanks!",
+            allowOutsideClick: false,
+            didOpen: () => {
+              Swal.showLoading();
+            },
+          });
+        }
+        countries.length === 0 && (await dispatch(fetchCountriesInfo()));
+
+        activities.length === 0 && (await dispatch(getActivities()));
+      } catch (error) {
+        throw Error(error);
+      } finally {
+        Swal.close();
+      }
+
+      // setLoader(false);
 
       // setTimeout(() => {
       // }, 1500);
@@ -77,7 +96,7 @@ function Home() {
 
   return (
     <>
-      {loader && <Loader />}
+      {/* {loader && <Loader />} */}
       <div className={homeContainer}>
         <span onClick={toggleFilterOptions}>Filter</span>
         {countries.length !== 250 ? (
